4f860864e474e1d4e9f3ab869101efd9bec643a3
opilane <opilane@HP-CZC0359NNV>
2023-02-14 09:52:50 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var states = new Dictionary<string, string>();
+
+            states.Add("Estonia", "EST");
+            states.Add("Latvia", "LAT");
+            states.Add("Finland", "FIN");
+            states.Add("Sweden", "SWE");
+        
+            foreach (var state in states)
+            {
+                Console.WriteLine($"{state.Key} - {state.Value}");
+            }
+            Console.WriteLine("---------------");
+            Console.WriteLine(states["Estonia"]);
+            Console.Write(states["Latvia"]);
+        }
+    }
+}
